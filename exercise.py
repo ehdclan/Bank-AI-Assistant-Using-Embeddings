5fbb1ba68009493e51452beaa428c2913312ddca
ehdclan <ebukaharry66@gmail.com>
2025-11-14 16:14:33 +0100

    added more functionalities

--- exercise.py
+++ exercise.py
@@ -16,16 +16,11 @@ client = AzureOpenAI(
         api_key=os.getenv("API_KEY"),
 )
 
-# docs = {
-#     "bank_policy.txt": "Transfers above NGN5,000,000 require manager approval.",
-#     "loan_terms.txt": "Maximum personal loans is NGN2,000,000",
-#     "faq.txt": "You can reset your password in the mobile app settings"
-# }
-
 docs = {
-    "bank_policy.txt": lambda: load_file("bank_policy.txt"),
-    "loan_terms.txt": lambda: load_file("loan_terms.txt"),
-    "faq.txt": lambda: load_file("faq.txt")
+    "digital_fraud_patterns.txt": lambda: load_file("digital_fraud_patterns.txt"),
+    "account_fraud_patterns.txt": lambda: load_file("account_fraud_patterns.txt"),
+    "transaction_fraud_patterns.txt": lambda: load_file("transaction_fraud_patterns.txt"),
+    "detection_prevention_patterns.txt": lambda: load_file("detection_prevention_patterns.txt")
 }
 
 embeddings = {}
@@ -37,10 +32,9 @@ for key, value in docs.items():
     )
     embeddings[key] = np.array(res.data[0].embedding)
 
-    question = "What is the maximum amount for personal loans?"
-
+    user_input = input("Enter your question about bank fraud patterns: ")
     q_embeded = np.array(client.embeddings.create(
-        input=question,
+        input=user_input,
         model="text-embedding-3-small"
     ).data[0].embedding)
 
@@ -53,7 +47,7 @@ for key, value in docs.items():
     for name, emb in embeddings.items():
         score = cosine_similarity(q_embeded, emb)
         if score > best_score:
-            best_score = score
+            first_score = score
             best_doc = name
 
     print(f"Best matching document: {best_doc} with score {best_score:.2f}")
@@ -62,8 +56,8 @@ for key, value in docs.items():
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {"role": "system", "content": "You are a helpful banking assistant."},
-            {"role": "user", "content": f"Context: {context}\n\nQuestion: {question}"}
+            {"role": "system", "content": "You are a helpful banking assistant. An expert in fraud detection and prevention. Provide information based on the context provided."},
+            {"role": "user", "content": f"Context: {context}\n\nQuestion: {user_input}"}
         ]
     )
     print("Response:", response.choices[0].message.content)
